f71fae3ddab65ab678fbf5c19957017be4fe6cd6
randmaru <git@randma.ru>
2021-04-24 08:50:06 +0300

    decided

---
+++ 002kata/solution.js
@@ -0,0 +1,15 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+// Напишите функцию repeatStr, которая повторяет заданную строку ровно n раз.
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+// Отлично, гуглим умножение строки.
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+// Там был пример: ({ toString: () => 'абв', repeat: String.prototype.repeat }).repeat(2);
+// Заменяем в нём 'абв' на s, а чисто повторений (2) на n.
+
+function repeatStr (n, s) {
+  return ({ toString: () => s, repeat: String.prototype.repeat }).repeat(n);
+}
+
+// Тест проходит. Задачка решена :)
