6e047089248d119b2ef9839b48c40cb20b8ee086
randmaru <git@randma.ru>
2021-04-24 10:19:29 +0300

    formatting

--- 002kata/solution.js
+++ 002kata/solution.js
@@ -8,8 +8,11 @@
 // Там был пример: ({ toString: () => 'абв', repeat: String.prototype.repeat }).repeat(2);
 // Заменяем в нём 'абв' на s, а чисто повторений (2) на n.
 
-function repeatStr (n, s) {
+function repeatStr(n, s) {
   return ({ toString: () => s, repeat: String.prototype.repeat }).repeat(n);
 }
 
 // Тест проходит. Задачка решена :)
+
+// Оказалось, есть более лаконичное решение данной задачи.
+// return s.repeat(n);
